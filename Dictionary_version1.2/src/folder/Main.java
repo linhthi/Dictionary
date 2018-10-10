26fa14a0601ecd919902a993d2b1a58b04c68512
linhht-uet <hoanglinh831999@gmail.com>
2018-10-10 22:31:32 +0700

    Update Dictionary_version1.2/src/folder/Main.java

--- Dictionary_version1.2/src/folder/Main.java
+++ Dictionary_version1.2/src/folder/Main.java
@@ -2,10 +2,11 @@
 package folder;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         DictionaryCommandLine newCommandLine = new DictionaryCommandLine();
         newCommandLine.dictionaryBasic();
         newCommandLine.dictionaryAdvanced();
