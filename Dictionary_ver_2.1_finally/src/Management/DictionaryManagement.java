f50337bd86a357ad8718d118ac9dbcc465cbc3d6
Hoang Thi Linh <hoanglinh831999@gmail.com>
2019-01-16 14:45:59 +0700

    Update DictionaryManagement.java

--- Dictionary_ver_2.1_finally/src/Management/DictionaryManagement.java
+++ Dictionary_ver_2.1_finally/src/Management/DictionaryManagement.java
@@ -17,35 +17,43 @@ public class DictionaryManagement {
     
     // Them tu tu trong file
     public void insertFromFile(String pathFile) throws FileNotFoundException {
+        
         Scanner scan = new Scanner(new File(pathFile));
-        while (scan.hasNext()){
+        
+        while (scan.hasNext()) {
+            
             String target = scan.nextLine();
             String explain = scan.nextLine();
+            
             Dictionary.hashMap.put(target, explain);
-            //model.addElement(target);
+            
         }
     }
     
     // Them tu vao trong tu dien
     public void addWord(String target, String explain) {
+        
         Dictionary.hashMap.put(target, explain);
-        //model.addElement(target);
+        
     }
     
     // Xoa tu ra khoi tu dien
     public void removeWord(String target) {
+        
         Dictionary.hashMap.remove(target);
-        //model.removeElement(target);
+        
     }
     
     // Sua tu trong tu dien
     public void modifyWord(String target, String explain) {
+        
         Dictionary.hashMap.replace(target, explain);
         
     }
     
     //Tim mot tu trong tu dien
     public boolean dictionaryLookUp(String target) {
+        
         return (Dictionary.hashMap.get(target) != null);
 
     }
@@ -55,15 +63,20 @@ public class DictionaryManagement {
     public void dictionaryExportToFile() throws FileNotFoundException, IOException {
         
         FileOutputStream fout = new FileOutputStream("data_Now.txt");
+        
         try (BufferedOutputStream bout = new BufferedOutputStream(fout)) {
+            
             Set set = Dictionary.hashMap.entrySet();
             Iterator iterator = set.iterator();
+            
             while(iterator.hasNext()) {
+                
                 Map.Entry mentry = (Map.Entry)iterator.next();
                 String line = mentry.getKey() + "\n" + mentry.getValue();
                 bout.write(line.getBytes());
                 bout.write(System.lineSeparator().getBytes());
+                
             }
         }
-}
+    }
 }
